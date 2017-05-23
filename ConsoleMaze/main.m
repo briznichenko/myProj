f07e0fd1527ca91294cc29b0f1311a089acb20f4
user <user@users-Mac-mini.local>
2017-05-23 13:07:36 +0300

    Initial Commit

---
+++ ConsoleMaze/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ConsoleMaze
+//
+//  Created by user on 5/23/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
